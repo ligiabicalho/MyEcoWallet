cfeff69319f1952e53a38079122a3ddece58045d
Lígia Bicalho <ligiabicalho@gmail.com>
2022-11-01 21:08:36 -0300

    Requisito 7 - valores da tabela são renderizados de acordo com as despesas salvas no estado global

--- src/components/Table.js
+++ src/components/Table.js
@@ -5,40 +5,55 @@ import '../styles/Table.css';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
     return (
       <div className="table-page">
-        Table
+        <h2>Table</h2>
         <table className="grid-table">
-          <tr>
-            <th>Descrição</th>
-            <th>Tag</th>
-            <th>Método de pagamento</th>
-            <th>Valor</th>
-            <th>Moeda</th>
-            <th>Câmbio utilizado</th>
-            <th>Valor convertido</th>
-            <th>Moeda de conversão</th>
-            <th>Editar/Excluir</th>
-          </tr>
-          <tr>
-            <td>cell1_1</td>
-            <td>cell2_1</td>
-            <td>cell3_1</td>
-            <td>cell4_1</td>
-            <td>cell5_1</td>
-            <td>cell6_1</td>
-            <td>cell7_1</td>
-            <td>cell8_1</td>
-            <td>cell9_1</td>
-          </tr>
+          <thead>
+            <tr>
+              <th>Descrição</th>
+              <th>Tag</th>
+              <th>Método de pagamento</th>
+              <th>Valor</th>
+              <th>Moeda</th>
+              <th>Câmbio utilizado</th>
+              <th>Valor convertido</th>
+              <th>Moeda de conversão</th>
+              <th>Editar/Excluir</th>
+            </tr>
+          </thead>
+          <tbody>
+            {console.log('prop expenses', expenses)}
+            {expenses.length > 0
+            && expenses.map((expense, i) => (
+              <tr key={ i }>
+                <td>{expense.description}</td>
+                <td>{expense.tag}</td>
+                <td>{expense.method}</td>
+                <td>{parseFloat(expense.value).toFixed(2)}</td>
+                <td>{expense.exchangeRates[expense.currency].name}</td>
+                <td>
+                  {parseFloat(expense.exchangeRates[expense.currency].ask)
+                    .toFixed(2)}
+                </td>
+                <td>
+                  {parseFloat(expense.value * expense.exchangeRates[expense.currency]
+                    .ask).toFixed(2)}
+                </td>
+                <td>Real</td>
+                <td><button type="button">Editar</button></td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </div>
     );
   }
 }
 
-const mapStateToProps = ({ wallet: expenses }) => ({
-  expenses,
+const mapStateToProps = ({ wallet }) => ({
+  expenses: wallet.expenses,
 });
 
 Table.propTypes = {
